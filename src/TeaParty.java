7e9b98b2ce482e2bb796b2a338cae0ae34f0f2b0
ilabstaff <ilabstaff@172.16.116.71>
2020-02-26 19:26:19 -0800

    min,teaParty

---
+++ src/TeaParty.java
@@ -0,0 +1,6 @@
+
+public class TeaParty {
+        public String welcome (String name, boolean isWoman, boolean isKnighted) {
+        
+        }
+}
--- src/_06_minion/Minion.java
+++ src/_06_minion/Minion.java
@@ -1,12 +1,79 @@
 package _06_minion;
-
 public class Minion {
 	private String name;
-	Minion(String name){
+	private int eyes;
+	private String color;
+	private String master;
+	
+	Minion(String name, int eyes, String color, String master){
 		this.name = name;
+		
+		this.eyes = eyes;
+		
+		this.color = color;
+		
+		this.master = master;
+		
 }
 	String getName(){
-		return name;
-	
+		
+		return this.name;
+	}
+	int geteyes(){
+		
+		return this.eyes;
+	}
+	String getcolor(){
+		
+		return this. color;
+	}
+	String getmaster(){
+		
+		return this. master;
+	}
+	void setName(String name) {
+		
+		this. name = name;
+	}
+	void seteyes(int eyes) {
+		
+		this. eyes = eyes;
+	}
+	void setcolor(String color) {
+		
+		this. color = color;
+	}
+	void setmaster(String master) {
+		
+		this. master = master;
+	}
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
