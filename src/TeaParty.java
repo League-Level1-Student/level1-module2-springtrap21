c27b3f94a0b40ce75009a4db8772049c3f74de17
ilabstaff <ilabstaff@172.16.116.69>
2020-03-04 19:04:57 -0800

    tea!

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -1,6 +1,22 @@
 
 public class TeaParty {
         public String welcome (String name, boolean isWoman, boolean isKnighted) {
+        	String message = "";
+        	message += "Hello";
+        if (isWoman && isKnighted) {
+			message += " Lady ";
+		}
+        if (isWoman && !isKnighted) {
+			message += " Ms. ";
+		}
+        if (isKnighted && !isWoman) {
+			message += " Sir ";
+		}
+        if (!isWoman && !isKnighted) {
+			message += " Mr. ";
+		}
+        message += name;
         
+        return message;
         }
 }
--- src/teaPartyTest.java
+++ src/teaPartyTest.java
@@ -1,4 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class teaPartyTest {
+	@Test
+	 public void test() {
+	     TeaParty teaParty = new TeaParty();
+	      String greeting = teaParty.welcome(null, false, false);
+	      assertEquals("Hello Ms. Austen", teaParty.welcome("Austen", true, false));
+	      assertEquals("Hello Mr. Orwell", teaParty.welcome("Orwell", false, false));
+	      assertEquals("Hello Sir Isaac Newton", teaParty.welcome("Isaac Newton", false, true));
+		}
+	}
 
-}
+
